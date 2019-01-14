7d20215cda93f3b841593f3fb5aff46b7b1b99ad
Zorana <zvukmiro@gmail.com>
2019-01-13 22:15:18 -0800

    rendering template added

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,5 +23,5 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1 number xx: TODO"
-
+#    return "Project 1 number xx: TODO"
+    return render_template("index.html")
