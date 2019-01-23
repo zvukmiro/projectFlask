1a4f2f55541134ff0da70aaaf3f313a473ef62b9
Zorana <zvukmiro@gmail.com>
2019-01-23 15:44:19 -0800

    working version whole

--- application.py
+++ application.py
@@ -89,3 +89,97 @@ def login():
 def logout():
     session.pop('logged_in', None)
     return redirect(url_for('index'))
+
+# User searches for books (non isbn search and particular isbn search: could have one or more results)
+
+
+@app.route("/books", methods=['GET', 'POST'])
+def books():
+    books = None
+    if request.method == "POST":
+        isbn = request.form.get("isbn")
+        title = request.form.get("title")
+        author = request.form.get("author")
+        if isbn:
+            books = db.execute("SELECT * FROM books WHERE isbn=:isbn", {"isbn": isbn})
+            if books.rowcount == 0:
+                books = db.execute(
+                    "SELECT * FROM books WHERE isbn LIKE :isbn LIMIT 60", {"isbn": isbn+'%'})
+        elif title and author:
+            books = db.execute(
+                "SELECT * FROM books WHERE author=:author AND title=:title LIMIT 30", {"author": author, "title": title})
+        elif title:
+            books = db.execute("SELECT * FROM books WHERE title=:title LIMIT 30", {"title": title})
+            if books.rowcount == 0:
+                books = db.execute(
+                    "SELECT * FROM books WHERE title LIKE :title LIMIT 60", {"title": '%'+title+'%'})
+        elif author:
+            books = db.execute(
+                "SELECT * FROM books WHERE author=:author LIMIT 30", {"author": author})
+            if books.rowcount == 0:
+                books = db.execute(
+                    "SELECT * FROM books WHERE author LIKE :author LIMIT 60", {"author": '%'+author+'%'})
+        db.close()
+    return render_template("books.html", books=books)
+
+
+@app.route("/books/<isbn>")
+def book(isbn):
+    session['isbn'] = isbn
+    try:
+        book = db.execute("SELECT * FROM books WHERE isbn=:isbn", {"isbn": isbn})
+    except:
+        flash("The book with isbn, or title/author entered not found in the database.")
+        return redirect(url_for('error'))
+    else:
+        reviews = db.execute(
+            "SELECT ROUND(AVG(rating), 1) FROM reviews WHERE rw_isbn=:isbn", {"isbn": isbn})
+        if reviews.rowcount == 1:
+            review = reviews.first()[0]
+        res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                           params={"key": "SlE9tBsqoV9Pzyb7ENbHXg", "isbns": isbn})
+        review2 = (res.json())['books'][0]['average_rating']
+        return render_template("book.html", book=book, review=review, review2=review2)
+    finally:
+        db.close()
+
+
+@app.route("/review", methods=['GET', 'POST'])
+def review():
+    if request.method == "POST":
+        isbn = session['isbn']
+        username = session['username']
+        review = request.form.get("review")
+        if (review.isdigit() or review.replace('.', '').isdigit()) and (float(review) > 0.99 and float(review) < 5.1):
+            try:
+                result = db.execute(
+                    "SELECT * FROM reviews WHERE rw_user=:username AND rw_isbn=:isbn", {"username": username, "isbn": isbn})
+                if (result.rowcount) > 0:
+                    flash("Duplicate review for the same book not accepted.")
+                    return redirect(url_for('error'))
+                review = float(review)
+                res = db.execute("INSERT INTO reviews (rw_isbn, rw_user, rating) VALUES (:bookNum, :person, :numb)", {
+                                 "bookNum": isbn, "person": username, "numb": review})
+                db.commit()
+                reviewCnt = db.execute(
+                    "SELECT * FROM reviews WHERE rw_user=:username", {"username": username})
+                return redirect(url_for('success'))
+            except:
+                flash("Your review did not post.")
+                return redirect(url_for('error'))
+        else:
+            flash('The review is 1 to 5 stars. Please enter numbers btw 1.0 and 5.0.')
+            return redirect(url_for('error'))
+
+    return redirect(url_for('error'))
+
+
+@app.route("/success")
+def success():
+    username = session['username']
+    return render_template("success.html", username=username)
+
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
